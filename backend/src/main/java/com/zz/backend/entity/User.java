a6d052c9f87ea86fbf369f25e1e41501337036f1
Daydreamer-Zhen <zz597375428@gmail.com>
2020-03-17 02:16:30 +0800

    lombok User entity

--- backend/src/main/java/com/zz/backend/entity/User.java
+++ backend/src/main/java/com/zz/backend/entity/User.java
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-03-15 01:55:53
  * @LastEditors: zhen
- * @LastEditTime: 2020-03-16 20:24:40
+ * @LastEditTime: 2020-03-17 02:13:23
  * @FilePath: /decentralized-voting/backend/src/main/java/com/zz/backend/entity/User.java
  * @Description: 
  */
@@ -11,59 +11,39 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.extension.activerecord.Model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @TableName("user")
 public class User {
+   
     private static final long serialVersionUID = 1L;
     //主键
+    @Getter
+    @Setter
     @TableId(value="id", type = IdType.AUTO)
     private int id;
     //帐号
+    @Getter
+    @Setter
     @TableField("account")
     private String account;
     //登录密码
+    @Getter
+    @Setter
     @TableField("password")
     private String password;
-
-    // @Override
-	// protected Serializable pkVal() {
-	// 	return this.id;
-    // }
-    
-    public User() {
-    }
-
-    public User(int id, String account, String password) {
-        this.id = id;
-        this.account = account;
-        this.password = password;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getAccount() {
-        return this.account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    //姓名
+    @Getter
+    @Setter
+    @TableField("name")
+    private String name;
+    //所属组织
+    @Getter
+    @Setter
+    @TableField("organaztion")
+    private String organization;
 }
